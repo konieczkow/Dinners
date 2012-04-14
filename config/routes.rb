fdf28879c5b8d00947ae389c64c5342dd681fa8c
Maciej Gajewski <gajewski.mm@gmail.com>
2012-04-14 16:36:53 +0200

    Added login to routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Dinners::Application.routes.draw do
   resources :static
 
+  match '/login', to: 'sessions#new'
   match '/auth/:provider/callback', to: 'sessions#create'
 
   root to: 'Home#index'
