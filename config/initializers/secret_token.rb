576eca7ec3c667b90424d4e7ffb36977108d1dcc
Maciej Gajewski <gajewski.mm@gmail.com>
2012-04-14 12:25:03 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dinners::Application.config.secret_token = '3ce1bc628b71efc2683d6bbd144e34761f89ecf1e3809c2787c31292881ec067e38114803b49c29e24e686c8936fa0af6ec248b7118fd65d76d2cbe04531f5c7'
