b51ddce121f29d0a2ab4a03c63b563138984c053
Karol Sarnacki <sodercober@gmail.com>
2012-04-14 15:56:35 +0200

    Validate user_id presence in Event

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,7 @@ class Event < ActiveRecord::Base
   belongs_to :user, inverse_of: :events, counter_cache: true
   has_many :places, inverse_of: :event, dependent: :destroy
 
+  validates :user_id,
+            presence: true
+
 end
