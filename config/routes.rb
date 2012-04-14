0e435abfbc1e92667721c042dd4e5edcdc348f0a
Paweł Kowalski <pavelloz@gmail.com>
2012-04-14 13:33:28 +0200

    trying haml

--- config/routes.rb
+++ config/routes.rb
@@ -47,7 +47,7 @@ Dinners::Application.routes.draw do
   #   end
 
   # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
+  # just remember to delete public/index-default.html.
   # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
