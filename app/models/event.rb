9b1bc3e7e43b41a65b86af0486d2d47832d7ceca
Karol Sarnacki <sodercober@gmail.com>
2012-04-14 15:55:18 +0200

    Add event_id foreign key to Places

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ActiveRecord::Base
 
   belongs_to :user, inverse_of: :events, counter_cache: true
-  has_many :places
+  has_many :places, inverse_of: :event, dependent: :destroy
 
 end
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,6 +1,9 @@
 class Place < ActiveRecord::Base
 
-  belongs_to :event
+  belongs_to :event, inverse_of: :places, counter_cache: true
   has_many :votes
+
+  validates :event_id,
+            presence: true
 
 end
